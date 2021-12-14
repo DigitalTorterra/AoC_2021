348b79f0c3bd3394e02c835e6db5a0cf6a588657
Patrick Faley <patrick.faley@gmail.com>
2021-12-13 18:09:00 -0600

    Finished day 1

--- day1.hs
+++ day1.hs
@@ -1,10 +1,18 @@
 
+slidingWindow :: (Num a) => [a] -> [a]
+slidingWindow xs = map (\(a, b, c) -> a + b + c) (zip3 xs (tail xs) (tail . tail $ xs))
 
+countIncreases :: (Ord a) => [a] -> Int
+countIncreases xs = length . (filter (\(a, b) -> a < b)) $ zip xs (tail xs)
 
 
 -- Main function
 main = do
   -- Parse the data
   contents <- getContents
-  let in_data = lines contents
-  putStrLn . show . length $ in_data
+  let in_data = map read (lines contents) :: [Integer]
+      part_1 = countIncreases in_data
+      part_2 = countIncreases . slidingWindow $ in_data
+
+  putStrLn $ "Part 1: " ++ (show part_1)
+  putStrLn $ "Part 2: " ++ (show part_2)
